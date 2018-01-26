81b54b22489e51ab687bb014b2ccdf5a7329019e
Boris Rasin <boris@pointx.org>
2018-01-26 15:12:43 -0800

    add support for sequence containers (using std::find)

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # find
-Advanced find() for associative containers:
+Advanced find() function for any container.
+Requires C++17 compiler.
 
 ```C++
-template <typename C>
-auto find(C& c, const typename C::key_type& key);
+template <typename C, typename Key>
+auto find(C& c, const Key& key);
 ```
 
 If element is found, return value evaluates to true and contains iterator to found element. If element is not found, return value evaluates to false and contains hint iterator for insert/emplace.
@@ -44,4 +45,9 @@ boost::multi_index::hashed_unique
 boost::multi_index::hashed_non_unique
 boost::multi_index::ranked_unique
 boost::multi_index::ranked_non_unique
+
+built-in array
+std::vector
+std::array
+std::list
 ```
--- find.h
+++ find.h
@@ -9,142 +9,102 @@ Copyright (c) 2016-2017 Boris Rasin (boris@pointx.org)
 #ifndef SKY_CONTAINER_FIND_H
 #define SKY_CONTAINER_FIND_H
 
-#include <type_traits>
+#include <algorithm>
+#include <experimental/type_traits>
 
 namespace sky {
 namespace detail {
 
-template <typename...>
-using void_t = void;
+template<typename T>
+using mapped_type_t = typename T::mapped_type;
 
-template <typename, typename = void_t<>>
-struct has_mapped_type : std::false_type {};
-
-template <typename T>
-struct has_mapped_type<T, void_t<typename T::mapped_type>> : std::true_type {};
-
-template <typename, typename = void_t<>>
-struct has_key_from_value : std::false_type {};
-
-template <typename T>
-struct has_key_from_value<T, void_t<typename T::key_from_value>> : std::true_type {};
-
-template <typename, typename = void_t<>>
-struct has_lower_bound : std::false_type {};
-
-template <typename T>
-struct has_lower_bound<T, void_t<decltype(std::declval<T>().lower_bound(std::declval<typename T::key_type>()))>> : std::true_type {};
+template<typename T>
+using key_from_value_t = typename T::key_from_value;
 
 // 3 key/value extractors:
 //   map (mapped_type)
 //   multi_index (key_from_value)
-//   set (none of the above)
+//   set/vector/array, etc. (none of the above)
 
-template <typename C, typename = void>
-struct extract;
-
-template <typename C>
-struct extract<C, std::enable_if_t<has_mapped_type<C>::value>>
+template <typename C, typename Iterator>
+inline decltype(auto) extract_key(C& c, Iterator& i)
 {
-	template <typename Iterator>
-	static decltype(auto) key(C& c, Iterator& i)
-	{
-		return i->first;
-	}
+    if constexpr (std::experimental::is_detected_v<mapped_type_t, C>)
+    {
+        return i->first;
+    }
+    else if constexpr (std::experimental::is_detected_v<key_from_value_t, C>)
+    {
+        return c.key_extractor()(*i);
+    }
+    else
+    {
+        return *i;
+    }
+}
 
-	template <typename Iterator>
-	static auto& value(Iterator& i)
-	{
-		return i->second;
-	}
-};
-
-template <typename C>
-struct extract<C, std::enable_if_t<has_key_from_value<C>::value>>
+template <typename C, typename Iterator>
+inline auto& extract_value(Iterator& i)
 {
-	template <typename Iterator>
-	static decltype(auto) key(C& c, Iterator& i)
-	{
-		return c.key_extractor()(*i);
-	}
-
-	template <typename Iterator>
-	static auto& value(Iterator& i)
-	{
-		return *i;
-	}
-};
-
-template <typename C>
-struct extract<C, std::enable_if_t<!has_mapped_type<C>::value && !has_key_from_value<C>::value>>
-{
-	template <typename Iterator>
-	static decltype(auto) key(C& c, Iterator& i)
-	{
-		return *i;
-	}
-
-	template <typename Iterator>
-	static auto& value(Iterator& i)
-	{
-		return *i;
-	}
-};
+    if constexpr (std::experimental::is_detected_v<mapped_type_t, C>)
+    {
+        return i->second;
+    }
+    else
+    {
+        return *i;
+    }
+}
 
 template <typename C, typename Iterator>
 class conditional_iterator
 {
 public:
+    conditional_iterator(Iterator i, bool condition)
+        : i(i), condition(condition) {}
 
-	conditional_iterator(Iterator i, bool condition) : i(i), condition(condition) {}
+    explicit operator bool() const { return condition; }
+    operator typename C::const_iterator() const { return i; }
 
-	explicit operator bool () const { return condition; }
-	operator typename C::const_iterator () const { return i; }
+    auto operator-> () const { return &extract_value<C>(i); }
+    auto& operator*() const { return extract_value<C>(i); }
 
-	auto operator -> () const { return &extract<C>::value(i); }
-	auto& operator * () const { return extract<C>::value(i); }
-
-	conditional_iterator operator ! () const { return { i, !condition }; }
+    conditional_iterator operator!() const { return {i, !condition}; }
 
 private:
-
-	Iterator i;
-	bool condition;
-
+    Iterator i;
+    bool condition;
 };
 
-template <typename C, typename = void>
-struct find;
+template<typename T>
+using lower_bound_t = decltype(std::declval<T>().lower_bound(std::declval<typename T::key_type>()));
 
-template <typename C>
-struct find<C, std::enable_if_t<has_lower_bound<C>::value>>
-{
-	static auto call(C& c, const typename C::key_type& key) -> conditional_iterator<C, decltype(c.lower_bound(key))>
-	{
-		auto it = c.lower_bound(key);
-		return { it, it != c.end() && !(c.key_comp()(key, extract<C>::key(c, it))) };
-	}
-};
-
-template <typename C>
-struct find<C, std::enable_if_t<!has_lower_bound<C>::value>>
-{
-	static auto call(C& c, const typename C::key_type& key) -> conditional_iterator<C, decltype(c.find(key))>
-	{
-		auto it = c.find(key);
-		return { it, it != c.end() };
-	}
-};
+template<typename T>
+using find_t = decltype(std::declval<T>().find(std::declval<typename T::key_type>()));
 
 } // namespace detail
 
 // If element is found, return value evaluates to true and contains iterator to found element.
 // If element is not found, return value evaluates to false and contains hint iterator for insert/emplace.
 
-template <typename C>
-inline auto find(C& c, const typename C::key_type& key)
+template <typename C, typename Key>
+inline auto find(C& c, const Key& key)
 {
-	return detail::find<C>::call(c, key);
+    if constexpr (std::experimental::is_detected_v<detail::lower_bound_t, C>)
+    {
+        auto it = c.lower_bound(key);
+        return detail::conditional_iterator<C, decltype(c.lower_bound(key))> {it, it != c.end() && !(c.key_comp()(key, detail::extract_key(c, it)))};
+    }
+    else if constexpr (std::experimental::is_detected_v<detail::find_t, C>)
+    {
+        auto it = c.find(key);
+        return detail::conditional_iterator<C, decltype(c.find(key))> {it, it != c.end()};
+    }
+    else
+    {
+        auto it = std::find(std::begin(c), std::end(c), key);
+        return detail::conditional_iterator<C, decltype(std::find(std::begin(c), std::end(c), key))> {it, it != c.end()};
+    }
 }
 
 } // namespace sky
